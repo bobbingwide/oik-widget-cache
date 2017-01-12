edcea7456e11dd10247a94829f7e758c09ed36af
Herb Miller <herb@bobbingwide.com>
2017-01-12 14:45:20 +0000

    Issue #1 - refactor before adding dependencies cache logic

--- class-oik-widget-cache.php
+++ class-oik-widget-cache.php
@@ -1,4 +1,4 @@
-<?php // (C) Copyright Bobbing Wide 2016
+<?php // (C) Copyright Bobbing Wide 2016, 2017
 
 /**
  * oik_widget_cache class
@@ -17,10 +17,20 @@
  */
 class oik_widget_cache {
 
+	public $timer_start;
+	public $cache_key;
+	public $dependencies;
+	public $cached_widget;
+	public $dependencies_cache;
+
 	// Store IDs of widgets to include from cache
 	private $included_ids = array();
 
 
+	/**
+	 * Constructor for oik_widget_cache
+	 * 
+	 */
 	protected function __construct() {
 
 		// Enable localization
@@ -40,6 +50,9 @@ class oik_widget_cache {
 
 		// Save widget cache settings
 		add_action( 'sidebar_admin_setup', array( $this, 'save_widget_controls' ) );
+		
+		
+		add_action( "oik_loaded", array( $this, "oik_widget_cache_oik_loaded" ) );
 
 	}
 
@@ -56,80 +69,96 @@ class oik_widget_cache {
 	}
 
 
+	/** 
+	 * Implements 'plugins_loaded' 
+	 */
 	function init_l10n() {
 
 		//load_plugin_textdomain( 'oik-widget-cache', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
 	}
-
-
+	
+	/** 
+	 * Implements 'init' hook
+	 * 
+	 * Determines which widgets are to be cached.
+	 */
 	function init() {
-
 		$this->included_ids = (array) get_option( 'cache-widgets-included', array() );
-
 	}
-
-
+	
+	/**
+	 * Implement 'widget_display_callback' filter
+	 * 
+	 * @param array $instance
+	 * @param WP_Widget $widget_object
+	 * @param array $args
+	 * @return false|$instance - false when we're doing it, $instance otherwise
+	 */
 	function widget_callback( $instance, $widget_object, $args ) {
 		
 		// Don't return the widget
-		if ( false === $instance || ! is_subclass_of( $widget_object, 'WP_Widget' ) )
+		if ( false === $instance || ! is_subclass_of( $widget_object, 'WP_Widget' ) ) {
 			return $instance;
+		}
 
-		if ( !in_array( $widget_object->id, $this->included_ids ) )
+		if ( !in_array( $widget_object->id, $this->included_ids ) ) {
 			return $instance;
+		}
 
-		$timer_start = microtime(true);
+		$this->timer_start = microtime(true);
+		$this->set_cache_key( $widget_object->id );
 
-		$cache_key = sprintf( 'cwdgt-%s',
-				md5( $widget_object->id . get_option( 'cache-widgets-version', 1 ) )
-			);
+		$this->get_cached_widget();
 
-		$cached_widget = get_transient( $cache_key );
-
-		if ( empty( $cached_widget ) ) {
-
+		if ( empty( $this->cached_widget ) ) {
 			ob_start();
-				$widget_object->widget( $args, $instance );
-				$cached_widget = ob_get_contents();
+			$widget_object->widget( $args, $instance );
+			$this->cached_widget = ob_get_contents();
 			ob_end_clean();
-
-			set_transient(
-				$cache_key,
-				$cached_widget,
-				apply_filters( 'widget_output_cache_ttl', 60 * 12, $args )
-			);
-
+			$this->determine_dependencies();
+			$this->cache_widget( $args );
+			$this->display_widget( "into cache" );
+		} else {
+			$this->replay_dependencies();
+			$this->display_widget( "from cache" );
+		}
+		// We already echoed the widget, so return false
+		return false;
+	}
+	
+	/**
+	 * Displays the widget.
+	 * 
+	 * @param string $widget_contents
+	 * @param string $method
+	 
 			printf(
 				'%s <!-- Stored in widget cache in %s seconds (%s) -->',
 				$cached_widget,
 				round( microtime(true) - $timer_start, 4 ),
 				$cache_key
 			);
-
-		} else {
-
+			
+			
 			printf(
-				'%s <!-- From widget cache in %s seconds (%s) -->',
+				'%s <!--  in %s seconds (%s) -->',
 				$cached_widget,
 				round( microtime(true) - $timer_start, 4 ),
 				$cache_key
 			);
 
-		}
-
-		// We already echoed the widget, so return false
-		return false;
-		
-	}
-
+	 */
+	function display_widget( $method ) {
+		$elapsed = microtime( true ) - $this->timer_start;
+		$elapsed = number_format( $elapsed, 6 );
+		printf( "<!-- Method: %s, ID: %s, Elapsed %s -->", $method, $this->cache_key, $elapsed );
+		echo $this->cached_widget;
+	}	 	
 
 	function cache_bump( $instance ) {
-
 		update_option( 'cache-widgets-version', time() );
-
 		return $instance;
-
 	}
 
 
@@ -168,7 +197,7 @@ class oik_widget_cache {
 
 		} elseif ( in_array( $widget_id, $this->included_ids ) ) {
 
-			// Widget is being removed, remove it from exclusions too
+			// Widget is being removed, remove it from inclusion too
 			$include_pos_key = array_search( $widget_id, $this->included_ids );
 			unset( $this->included_ids[ $include_pos_key ] );
 
@@ -179,6 +208,74 @@ class oik_widget_cache {
 		update_option( 'cache-widgets-included', $this->included_ids );
 
 	}
-
+	
+	/**
+	 * Sets the cache key.
+	 *
+	 * Sets a unique cache key for the widget ID and cache widgets version
+	 */
+	function set_cache_key( $id ) {
+		$id_version = $id;
+		$id_version .= get_option( 'cache-widgets-version', 1 );
+		$id_version = md5( $id_version );
+		$this->cache_key = 'cwdgt-' . $id_version;
+	}
+	
+	/**
+	 * Determines dependencies
+	 */
+	function determine_dependencies() {
+		$this->dependencies =  null;
+	}
+	
+	/**
+	 * Replays the dependencies saved when the widget was cached.
+	 *
+	 */
+	function replay_dependencies() {
+	
+	}
+	
+	/**
+	 * Caches the widget and dependencies
+	 * 
+	 * @TODO Add the dependencies stuff
+	 
+			set_transient(
+				$cache_key,
+				$cached_widget,
+				apply_filters( 'widget_output_cache_ttl', 60 * 12, $args )
+			);
+	 */
+	function cache_widget( $args ) {
+		$duration = apply_filters( 'widget_output_cache_ttl', 60 * 12, $args );
+    set_transient( $this->cache_key, $this->cached_widget, $duration );
+	}
+	
+	/**
+	 * Retrieves the cached widget. 
+	 */
+	
+	function get_cached_widget() {
+	
+		$this->cached_widget = get_transient( $this->cache_key );
+	}
+	
+	
+	/**
+	 * Implement "oik_loaded" 
+	 * 
+	 * When oik has been loaded we can try loading the class-dependencies-cache library
+	 * if this is available then we'll be able to cache the scripts and styles produced in widgets
+	 */
+	function oik_widget_cache_oik_loaded() {
+		oik_require_lib( "class-dependencies-cache" );
+		if ( !class_exists( "dependencies_cache" ) ) {
+			$this->dependencies_cache = null;
+		}	else {
+			$this->dependencies_cache = dependencies_cache::instance();
+		}					 
+}
+	
 
 }
